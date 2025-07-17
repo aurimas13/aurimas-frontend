c68b496ee85d458590bd2d2dd62d2f0664cc7e67
Aurimas 13 <aurimas.nausedas@proton.me>
2025-07-17 22:58:46 +0300

    Add files via upload

---
+++ src/data/blogCategories.ts
@@ -0,0 +1,26 @@
+export const blogCategories = {
+  'molecule-to-machine': {
+    title: 'Molecule To Machine',
+    description: 'Chemistry meets AI and technology',
+    originalUrl: 'https://moleculetomachine.substack.com/',
+    languages: 'English'
+  },
+  'grace-to-life': {
+    title: 'From Grace To Life',
+    description: 'Personal reflections and life stories',
+    originalUrl: 'https://fromgracetolife.substack.com/',
+    languages: 'English, French, Lithuanian'
+  },
+  'transcend-loneliness': {
+    title: 'Transcend Loneliness',
+    description: 'Overcoming isolation and building connections',
+    originalUrl: 'https://transcendloneliness.substack.com/',
+    languages: 'Early posts: English & Lithuanian, Later posts: All three languages'
+  },
+  'other-story-time': {
+    title: 'Other Story Time',
+    description: 'Creative stories and narratives',
+    originalUrl: 'https://otherstorytime.substack.com/',
+    languages: 'English, French, Lithuanian'
+  }
+};
---
+++ src/data/socialLinks.ts
@@ -0,0 +1,54 @@
+import { SocialLink } from '../types';
+
+export const socialLinks: SocialLink[] = [
+  {
+    platform: 'Facebook',
+    url: 'https://www.facebook.com/auris13/',
+    icon: 'facebook'
+  },
+  {
+    platform: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/aurimasnausedas/',
+    icon: 'linkedin'
+  },
+  {
+    platform: 'GitHub',
+    url: 'https://github.com/aurimas13',
+    icon: 'github'
+  },
+  {
+    platform: 'Twitter',
+    url: 'https://x.com/aurulia13',
+    icon: 'twitter'
+  },
+  {
+    platform: 'Instagram',
+    url: 'https://www.instagram.com/aurulia13/',
+    icon: 'instagram'
+  },
+  {
+    platform: 'YouTube',
+    url: 'https://www.youtube.com/@aurimas13',
+    icon: 'youtube'
+  },
+  {
+    platform: 'Unsplash',
+    url: 'https://unsplash.com/@aurimas13',
+    icon: 'camera'
+  },
+  {
+    platform: 'Spotify',
+    url: 'https://open.spotify.com/user/aurimas.n',
+    icon: 'music'
+  },
+  {
+    platform: 'Strava',
+    url: 'https://www.strava.com/athletes/aurimas13',
+    icon: 'activity'
+  },
+  {
+    platform: 'Ko-fi',
+    url: 'https://ko-fi.com/aurimas13',
+    icon: 'coffee'
+  }
+];
